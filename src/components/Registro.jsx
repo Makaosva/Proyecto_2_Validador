2b2a91c5d8d34fc72f62e411c29bfbd08da9ce74
Makaosva <maka.osses@gmail.com>
2024-06-01 23:53:42 -0400

    feat/intento 1 de agregar props

--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 import SocialButton from "./SocialButton";
+import Formulario from "./Formulario";
 
 function Registro() {
   return (
     <div className="card">
       <h1>Crear una cuenta</h1>
-      <SocialButton />
+      <div className="social">
+        <SocialButton
+          url="https://www.facebook.com/"
+          icono="../assets/img/001-facebook.png"
+          name="Facebook icon"
+        />
+        {/*}  <SocialButton
+          url="https://www.linkedin.com/"
+          logo="../assets/img/002-linkedin-logo.png"
+          name="LinkdIn icon"
+        />
+        <SocialButton
+          url="https://github.com"
+          logo="../assets/img/003-github.png"
+          name="Github icon"
+  />*/}
+      </div>
+
       <h3>O usa tu email para registrarte</h3>
-      {/*}  <Formulario />*/}
+      <Formulario />
     </div>
   );
 }
